703cc11680fc50c8e70f111c4452134e13a5e054
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-07-01 14:22:17 -0400

    abs

--- main.js
+++ main.js
@@ -19,7 +19,14 @@ for (let i=0; i <greeting.length; i++){
 return result
 }
 
+const absoluteValues= function(value){
+  let result =[];
+  for(let i=0; i< value.length; i++){
+result.push(Math.abs(value[i]))
 
+  }
+  return result
+}
 
 /*******************************************
 * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
